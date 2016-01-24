c6282a39a095f818ff1c791ac75d06366bb63ae4
Alexandre Cormier <contact@alexandrecormier.fr>
2016-01-24 14:43:22 +0100

    start coding outliner tab system , jquery/html/css

--- js/editor.js
+++ js/editor.js
@@ -34,6 +34,9 @@ var editorState = {
 		panel:{
 			menu : '',
 			outliner : '',
+			object_types:'',
+			images:'',
+			sounds:'',
 			properties : '',
 			creation:''
 		},
@@ -52,7 +55,11 @@ var editorState = {
 			this.panel.outliner = jQuery('#outliner')	
 			this.panel.properties = jQuery('#properties')
 			this.panel.creation = jQuery('#creation')
-			this.display_object_types_list();	
+			
+			this.build_outliner();
+			//this.display_object_types_list();	
+			//this.display_images_list();	
+			//this.display_sounds_list();	
 			this.displayed_game_object = game.add.sprite();
 			this.diplay_creation_panel();
 			this.display_menu_panel();
@@ -87,12 +94,37 @@ var editorState = {
 		
 		},
 		
+		
+		build_outliner:function(){
+		
+			jQuery(this.panel.outliner).empty();
+				
+			var tab_links_ul = jQuery('<ul></ul>');
+			jQuery(this.panel.outliner).append(tab_links_ul)
+			
+			for (var asset_type in GAME_ASSETS){
+			
+				var tab_link_li = jQuery('<li class = "tab_link" >'+asset_type+'</li>');
+				jQuery(tab_links_ul).append(tab_link_li )
+				
+			}
+		
+			for (var asset_type in GAME_ASSETS){
+			
+				var tab_content = jQuery('<div id = "'+asset_type+'" class = "tab_content"></div>');
+				jQuery(this.panel.outliner).append(tab_content)
+				this.panel[asset_type] = tab_content;
+
+			}		
+		
+		},
+		
 		display_object_types_list:function(){
 			
-			jQuery(this.panel.outliner).empty();
+			jQuery(this.panel.object_types).empty();
 
 			var GAME_OBJECT_TYPES_list = jQuery('<ul></ul>')
-			jQuery(this.panel.outliner).append(GAME_OBJECT_TYPES_list)
+			jQuery(this.panel.object_types).append(GAME_OBJECT_TYPES_list)
 			
 			var GUI = this;
 			
@@ -101,9 +133,9 @@ var editorState = {
 				var object_type_li =jQuery('<li class = "object_type_li"></li>')
 				var object_type_header =jQuery('<div class = "object_type_header" >'+GAME_OBJECT_TYPES[i].getName()+'</div>')
 				var object_type_options =jQuery('<div class = "object_type_options" ></div>')
-				
 				var object_type_select_button =jQuery('<button class = "select_button" name = "'+GAME_OBJECT_TYPES[i].getName()+'"> INSTANCIATE </button>')
-				jQuery(this.panel.outliner).append(object_type_li)
+				
+				jQuery(this.panel.object_types).append(object_type_li)
 				jQuery(object_type_li).append(object_type_header)
 				jQuery(object_type_li).append(object_type_options)
 				jQuery(object_type_options).append(object_type_select_button)
@@ -122,18 +154,78 @@ var editorState = {
 
 				})	
 				
-			}		
+			}				
 
-			var add_game_object_button = jQuery('<li><button class ="add_button">+</button></li>');
+			var add_object_types_button = jQuery('<li><button class ="add_button">+</button></li>');
 			
-			jQuery(this.panel.outliner).append(add_game_object_button)
+			jQuery(this.panel.object_types).append(add_object_types_button)
 			
-			jQuery(add_game_object_button).click(function(e){
+			jQuery(add_object_types_button).click(function(e){
 				
 				
 			})		
 			
 		},
+		
+		display_images_list:function(){
+		
+			jQuery(this.panel.images).empty();
+			
+			//var images_list_prop = new Property(GAME_ASSETS,GAME_ASSETS,'images',{input_type:'image',isArray:true,default_value:[]})
+			
+			//jQuery(this.panel.images).append(images_list_prop.create_jquery_object())
+		
+		},
+		
+		display_sounds_list:function(){
+		
+			jQuery(this.panel.sounds).empty();
+			
+			//var sounds_list_prop = new Property(GAME_ASSETS,GAME_ASSETS,'sounds',{input_type:'sound',isArray:true,default_value:[]})
+			
+			//jQuery(this.panel.sounds).append(sounds_list_prop.create_jquery_object())
+		
+		},
+		/*var IMAGES_LIST_ul = jQuery('<ul></ul>')
+		jQuery(this.panel.object_types).append(IMAGES_LIST_ul)
+		
+		var GUI = this;
+		
+		for (var i = 0 ; i<GAME_ASSETS.images.length; i++){
+			
+			var object_type_li =jQuery('<li class = "object_type_li"></li>')
+			var object_type_header =jQuery('<div class = "object_type_header" >'+GAME_ASSETS.images.name+'</div>')
+			var object_type_options =jQuery('<div class = "object_type_options" ></div>')
+			jQuery(this.panel.outliner).append(object_type_li)
+			jQuery(object_type_li).append(object_type_header)
+			jQuery(object_type_li).append(object_type_options)
+			jQuery(object_type_options).append(object_type_select_button)
+			
+			jQuery(object_type_select_button).click(function(e){
+				GUI.edit_object_type(e.target.name);
+			})
+			
+			var object_type_delete_button = jQuery('<button class ="delete_button" index = "'+i+'">x</button>');
+			jQuery(object_type_options).append(object_type_delete_button)
+			var context = this;
+			jQuery(object_type_delete_button).click(function(){
+
+				GAME_OBJECT_TYPES.splice(jQuery(this).attr('index'),1);
+				context.display_object_types_list();
+
+			})	
+			
+		}				
+
+		var add_game_object_button = jQuery('<li><button class ="add_button">+</button></li>');
+		
+		jQuery(this.panel.object_types).append(add_game_object_button)
+		
+		jQuery(add_game_object_button).click(function(e){
+			
+			
+		})	*/	
+		
 		
 		preview_object_type:function(){
 		
@@ -181,6 +273,26 @@ var editorState = {
 		},
 		
 		display_properties:function(){
+			
+			jQuery(this.panel.properties).empty();
+	
+			if(this.edited_object_type!= undefined){
+				
+				var object_type_data = this.edited_object_type.getModelData();
+
+				for(var p in this.edited_object_type.properties){
+					
+					this.edited_object_type.properties[p].link_to_GUI(this);
+					
+					jQuery(this.panel.properties).append(this.edited_object_type.properties[p].create_jquery_object())
+					
+				}
+			
+			}
+
+		},
+		
+		display_images:function(){
 			
 			jQuery(this.panel.properties).empty();
 	
